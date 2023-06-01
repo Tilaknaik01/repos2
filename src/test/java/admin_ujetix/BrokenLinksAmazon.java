4d7b7d336d5a6deb31c39d73a8e1f35a0cea717c
Tilaknaik01 <129579015+Tilaknaik01@users.noreply.github.com>
2023-06-01 11:22:39 +0530

    Update BrokenLinksAmazon.java

--- src/test/java/admin_ujetix/BrokenLinksAmazon.java
+++ src/test/java/admin_ujetix/BrokenLinksAmazon.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrokenLinksAmazon
 {
-	//class starts here
+	//class starts
 	public static void main(String[] args)
 	{
 		WebDriver driver= new ChromeDriver();
